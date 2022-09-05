478633ffd83dd650d0b194b74b6869102db2f749
Ayaz <Ayaz@DESKTOP-PN90N5M>
2022-09-05 18:42:39 +0530

    Add project files.

---
+++ EFCodeFirstApproach/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstApproach.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Mobile { get; set; }
+        public int Salary { get; set; }
+    }
+}
